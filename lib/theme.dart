2455569bb220ff867b11b4eb9fd6783d26fc25eb
Eros Menin <eros.menin@gmail.com>
2023-07-21 17:07:01 +0200

    added main and theme

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData burgerTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme.dark(
+    primary: Color.fromARGB(255, 242, 135, 5),
+    secondary: Color.fromARGB(255, 242, 116, 5),
+    tertiary: Color.fromARGB(255, 89, 50, 2),
+  ),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+  ),
+);
