146dc28e85b528c927fe8246bc63bc08b03b0354
ErosM04 <eros.menin@gmail.com>
2024-08-13 22:20:16 +0200

    turned Card page in StateLess widget and added doc

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -6,19 +6,17 @@ import 'package:cards_against_humanity/pages/master_page.dart';
 import 'package:cards_against_humanity/logic/logic.dart';
 import 'package:flutter/material.dart';
 
-class CardPage extends StatefulWidget {
+/// The page that is displayed after the **Game page** and shows the question card, with the answer/s previously
+/// selected by the player that fills the empty spots. The answer text color follows the [Theme] of the app.
+class CardPage extends StatelessWidget {
+  /// The object that manages the logic of the game.
   final CasualityManager random;
 
   const CardPage(this.random, {super.key});
 
   @override
-  State<CardPage> createState() => _CardPageState();
-}
-
-class _CardPageState extends State<CardPage> {
-  @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: const CustomAppBar(),
+      appBar: CustomAppBar(score: random.score, maxPoints: random.playedRounds),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 30),
@@ -32,13 +30,15 @@ class _CardPageState extends State<CardPage> {
                 children: [
                   _buildEndButton(
                     text: 'Ho perso',
-                    onPressed: () => widget.random.lost(),
+                    onPressed: () => random.lost(),
                     color: Colors.red,
+                    context: context,
                   ),
                   _buildEndButton(
                     text: 'Ho vinto',
-                    onPressed: () => widget.random.won(),
+                    onPressed: () => random.won(),
                     color: Colors.green,
+                    context: context,
                   ),
                 ],
               ),
@@ -48,20 +48,23 @@ class _CardPageState extends State<CardPage> {
       ));
 
   /// Used to build a the ``Ho perso`` and ``Ho vinto`` buttons.
-  Widget _buildEndButton(
-          {required String text,
-          required Function onPressed,
-          required Color color}) =>
+  Widget _buildEndButton({
+    required String text,
+    required Function onPressed,
+    required Color color,
+    required BuildContext context,
+  }) =>
       CustomButton(
-          text: text,
-          textColor: Colors.white,
-          fontSize: 20,
-          verticalInternalPadding: 12,
-          color: color,
-          onPressed: () {
-            onPressed();
-            _goToNewRound();
-          });
+        text: text,
+        textColor: Colors.white,
+        fontSize: 20,
+        verticalInternalPadding: 12,
+        color: color,
+        onPressed: () {
+          onPressed();
+          _goToNewRound(context);
+        },
+      );
 
   /// Used to build the complete card with the question and all the answer.
   /// Answers are in a different color.
@@ -69,7 +72,7 @@ class _CardPageState extends State<CardPage> {
         children: [
           CardAH(
             onClicked: () => null,
-            text: widget.random.question,
+            text: random.question,
             isClickable: false,
             answersList: CasualityManager.selectedCards,
           ),
@@ -86,23 +89,20 @@ class _CardPageState extends State<CardPage> {
       .replaceAll('[', '')
       .replaceAll(']', '');
 
-  /// Goes to a MasterGamePage if the next round is the round of the actual player, otherwise goes to a GamePage.
-  void _goToNewRound() {
-    widget.random.fillHand();
-    widget.random.drawQuestionCard();
+  /// Goes to a Master page if the next round is the round of the actual player, otherwise goes to the Game page.
+  void _goToNewRound(BuildContext context) {
+    random.fillHand();
+    random.drawQuestionCard();
 
     // The first condition is true if, for ex, there are 4 players and the actual player is the 4th (so it's his turn).
     // The secon condition is true if, for ex, the user is the player 1 and there are 4 player and is the round 5:
     // 5 % 4 = 1, which is equal to the number player number (1).
-    (((widget.random.round % widget.random.totalPlayers) == 0 &&
-                (widget.random.playerNumber == widget.random.totalPlayers)) ||
-            (widget.random.round % widget.random.totalPlayers) ==
-                widget.random.playerNumber)
-        ? Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (context) => MasterGamePage(widget.random)))
-        : Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => GamePage(widget.random)));
+    (((random.round % random.totalPlayers) == 0 &&
+                (random.playerNumber == random.totalPlayers)) ||
+            (random.round % random.totalPlayers) == random.playerNumber)
+        ? Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => MasterGamePage(random)))
+        : Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => GamePage(random)));
   }
 }
