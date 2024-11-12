1adfdab9fdbb09e396efdde723cb066a3e62fd65
ErosM04 <eros.menin@gmail.com>
2024-11-12 20:52:10 +0100

    modified card entity name

--- lib/core/entities/game/card.dart
+++ lib/core/entities/game/card.dart
@@ -1,10 +1,10 @@
 /// Stores the informations of a Cards Against Humanity's card.
-class CAHCard {
+class CardAH {
   /// The id of the card
   final int id;
 
   /// The text contained in the card. Either a question or an answer.
   final String text;
 
-  const CAHCard({required this.id, required this.text});
+  const CardAH({required this.id, required this.text});
 }
