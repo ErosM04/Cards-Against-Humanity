3ba6bcb17ca98ee2e1a86147f9988de46fedc64e
ErosM04 <eros.menin@gmail.com>
2025-03-22 22:22:38 +0100

    fixed typo in basic usecase

--- lib/core/usecases/usecase_param.dart
+++ lib/core/usecases/usecase_param.dart
@@ -2,7 +2,7 @@ import 'package:cards_against_humanity/core/error/failures.dart';
 import 'package:fpdart/fpdart.dart';
 
 /// Basic use case with parameters.
-abstract interface class UseCaseParam<SuccessType, Params> {
+abstract interface class UseCaseParam<SuccessType, Param> {
   /// Method used to perform the specific usecase that can return a failure or a success.
   Future<Either<Failure, SuccessType>> call();
 }
