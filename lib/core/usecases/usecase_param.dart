d0d9ab29c3179f828cb17f2b40a425110c4fa927
ErosM04 <eros.menin@gmail.com>
2025-03-23 19:02:53 +0100

    added syncronous usecases in core

--- lib/core/usecases/usecase_param.dart
+++ lib/core/usecases/usecase_param.dart
@@ -4,5 +4,11 @@ import 'package:fpdart/fpdart.dart';
 /// Basic use case with parameters.
 abstract interface class UseCaseParam<SuccessType, Param> {
   /// Method used to perform the specific usecase that can return a failure or a success.
+  Either<Failure, SuccessType> call(Param param);
+}
+
+/// Basic asynchronous use case with parameters.
+abstract interface class UseCaseAsyncParam<SuccessType, Param> {
+  /// Method used to perform the specific usecase that can return a failure or a success.
   Future<Either<Failure, SuccessType>> call(Param param);
 }
