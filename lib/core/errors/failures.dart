5072a9f586433539a242e77b24c746fc689e9616
ErosM04 <eros.menin@gmail.com>
2025-03-24 21:48:22 +0100

    changed range failure to data range failure in core

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,11 +1,11 @@
-/// Defines a basic failure in a process
+/// Defines a basic failure in a process.
 class Failure {
   final String message;
 
   Failure([this.message = '']);
 }
 
-/// Defines a failure when managing data
+/// Defines a failure when managing data.
 class DataFailure extends Failure {
   /// The type of data related to the failure
   Type dataType;
@@ -13,7 +13,8 @@ class DataFailure extends Failure {
   DataFailure(super.message, {required this.dataType});
 }
 
-/// Defines a failure related to values being out of a defined range
-class RangeFailure extends Failure {
-  RangeFailure() : super("Value/s out of range");
+/// Defines a failure related to values being out of a defined range.
+class DataRangeFailure extends DataFailure {
+  DataRangeFailure({required super.dataType})
+      : super("Value/s out of range or the container is empty");
 }
